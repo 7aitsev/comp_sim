cad8f467903cb98f8f3e5100a03a4b635c3df962
Maxim Zaitsev <7aitsev@outlook.com>
2015-02-11 15:45:00 +0300

    Create GenSettings.java

---
+++ comp/GenSettings.java
@@ -0,0 +1,16 @@
+package comp;
+
+import java.awt.Color;
+import java.awt.Font;
+
+public class GenSettings {
+  public static Font FONT = new Font("Consolas", Font.PLAIN, 12);  
+  public static Color BG_COLOR = new Color(135, 206, 235); //Color.GREEN;
+  public static Color NMBR_COLOR = Color.red;
+  public static Color VSEP_COLOR = new Color(100, 149, 237);
+  public static Color STATUS_BG_COLOR = VSEP_COLOR; //new Color(135, 206, 235);
+  public static Color STATUS_FG_COLOR = Color.white;
+  public static Color STATUS_SELTXT_COLOR = Color.black;
+  public static Color STATUS_SEL_COLOR = Color.white;
+  public static int ramAmount = 128;
+}
